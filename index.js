0f06312d5e3368c9b4956b62cb9b4a132b9eacff
Nevin John <nevinjohn63@gmail.com>
2026-01-30 14:56:48 +0530

    added commit to check i repo

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 // Middleware to parse JSON
 app.use(express.json());
 app.use(cors());
-// Root route
+
 app.get("/", (req, res) => {
   res.send("Hello from Express Server");
 });
